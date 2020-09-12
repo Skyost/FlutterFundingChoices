92a16b04a54814fc9263748b7093cce926eec003
Skyost <me@skyost.eu>
2020-09-12 23:46:52 +0200

    Finished iOS implementation.

--- ios/Classes/SwiftFlutterFundingChoicesPlugin.swift
+++ ios/Classes/SwiftFlutterFundingChoicesPlugin.swift
@@ -1,24 +1,69 @@
 import Flutter
 import UIKit
-import UserMessagingPlatform/UserMessagingPlatform
+import UserMessagingPlatform
 
 public class SwiftFlutterFundingChoicesPlugin: NSObject, FlutterPlugin {
-  /// The channel method name.
-  static let channelName: String = "flutter_funding_choices"
+    /// The channel method name.
+    static let channelName: String = "flutter_funding_choices"
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: channelName, binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterFundingChoicesPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "discovery.stop":
-        (browsers[id]?.delegate as! BonsoirServiceBrowserDelegate?)?.dispose()
-        result(true)
-    default:
-        result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: channelName, binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterFundingChoicesPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments: [String: Any?] = call.arguments as! [String: Any?]
+        switch call.method {
+        case "requestConsentInformation":
+            requestConsentInformation(tagForUnderAgeOfConsent: arguments["tagForUnderAgeOfConsent"] as! Bool, result: result)
+        case "showConsentForm": showConsentForm(result: result)
+        case "reset":
+            UMPConsentInformation.sharedInstance.reset()
+            result(true)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    /// Requests the consent information.
+    private func requestConsentInformation(tagForUnderAgeOfConsent: Bool, result: @escaping FlutterResult) {
+        let params = UMPRequestParameters()
+        params.tagForUnderAgeOfConsent = tagForUnderAgeOfConsent
+
+        UMPConsentInformation.sharedInstance.requestConsentInfoUpdate(with: params) { error in
+            if error == nil {
+                var consentInfo: [String: Any] = [:]
+                consentInfo["consentStatus"] = UMPConsentInformation.sharedInstance.consentStatus.rawValue
+                consentInfo["consentType"] = UMPConsentInformation.sharedInstance.consentType.rawValue
+                consentInfo["isConsentFormAvailable"] = UMPConsentInformation.sharedInstance.formStatus == UMPFormStatus.available
+                result(consentInfo)
+            } else {
+                result(FlutterError(code: "request_error", message: error!.localizedDescription, details: nil))
+            }
+        }
+    }
+
+    /// Shows the consent form.
+    private func showConsentForm(result: @escaping FlutterResult) {
+        let viewController = UIApplication.shared.keyWindow?.rootViewController
+        if viewController == nil {
+            result(FlutterError(code: "view_controller_error", message: "View controller is null.", details: nil))
+            return
+        }
+
+        UMPConsentForm.load { form, error in
+            if error == nil {
+                form!.present(from: viewController!) { dismissError in
+                    if dismissError == nil {
+                        result(true)
+                    } else {
+                        result(FlutterError(code: "form_dismiss_error", message: dismissError!.localizedDescription, details: nil))
+                    }
+                }
+            } else {
+                result(FlutterError(code: "form_error", message: error!.localizedDescription, details: nil))
+            }
+        }
+    }
 }
--- ios/flutter_funding_choices.podspec
+++ ios/flutter_funding_choices.podspec
@@ -4,18 +4,21 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'flutter_funding_choices'
-  s.version          = '0.0.1'
-  s.summary          = 'A new Flutter plugin.'
+  s.version          = '0.1.0'
+  s.summary          = 'The Flutter implementation of Funding Choices, a Google service that allows to request user consent for personalized ads in AdMob.'
   s.description      = <<-DESC
-A new Flutter plugin.
+The Flutter implementation of Funding Choices, a Google service that allows to request user consent for personalized ads in AdMob.
                        DESC
-  s.homepage         = 'http://example.com'
+  s.homepage         = 'https://pub.dev/packages/flutter_funding_choices'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Skyost' => 'me@skyost.eu' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
+  s.platform = :ios, '9.0'
+
+  s.dependency 'GoogleUserMessagingPlatform', '~> 1.1.0'
+  s.static_framework = true
 
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
